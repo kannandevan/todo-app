89ce470e087269015907cbb206566bc547ee9d11
Devannad <kannandevan007@gmail.com>
2025-06-30 22:27:28 +0530

    unwanted files removed, small style added , task status change logic added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from "axios";
+import Tasks from './components/Tasks'
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get('./data.json')
+      .then((res) => {
+        const allTasks = res.data;
+
+        setTasks(allTasks);
+
+      })
+      .catch((err) => {
+        console.error('failed to Fetch Data ', err)
+      })
+  }, []);
+
+  const handleStatusChange = (id) => {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+      return task
+    })
+    setTasks(updatedTasks);
+  }
+  const pendingTasks = tasks.filter((task) => !task.isCompleted);
+  const completedTasks = tasks.filter((task) => task.isCompleted);
   return (
-    <div className="App">
-     Hello Deva
+    <div className='app'>
+      {
+        pendingTasks.length != 0 ?
+          <div className='task-group'><div className="task-heading">Pending Tasks</div><Tasks tasks={pendingTasks} statusHandle={handleStatusChange} /></div> : ''
+      }
+      {
+        completedTasks.length != 0 ?
+          <div className='task-group'><div className="task-heading">Completed Tasks</div><Tasks tasks={completedTasks} statusHandle={handleStatusChange} /></div> : ''
+      }
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './Tasks.css'
+const Tasks = (props) => {
+    const { tasks,statusHandle } = props;
+
+    return (
+
+
+            <ul className="tasks">
+                {tasks.map((task,index) => (<li key={task.id} className="task">
+                    <div>{index+1}</div>
+                    <div className={task.isCompleted ? 'task-completd':''}>{task.name}</div>
+                    <div>
+                        <input 
+                        type="checkbox" 
+                        name="" id="" 
+                        checked={task.isCompleted}  
+                        onChange={() => statusHandle(task.id)}
+                        /></div>
+                </li>))}
+
+            </ul>
+
+
+    )
+}
+
+export default Tasks;
