ad9aef7c79509caea77ad27dcde60978e82cc9e7
Devannad <kannandevan007@gmail.com>
2026-01-07 22:29:55 +0530

    previous date hide from calendar

--- src/App.js
+++ src/App.js
@@ -161,6 +161,10 @@ function App() {
             <input
               type="date"
               value={date}
+              min={(() => {
+                const now = new Date();
+                return now.getFullYear() + '-' + String(now.getMonth() + 1).padStart(2, '0') + '-' + String(now.getDate()).padStart(2, '0');
+              })()}
               className='add-input date-input'
               onChange={(e) => setDate(e.target.value)}
             />
